92169ac53e6f71ef9b3f8ba3f82fffd5cea4d2eb
Nicole Sweeden <sweeden.nicole@gmail.com>
2022-12-08 15:33:16 -0700

    setting up

---
+++ js/movie-reviews.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+
+// Get all
+fetch("https://vast-organic-farm.glitch.me/movies").then(resp => resp.json()).then(data => console.log(data)); // get all
+
+
+
+
+
+
+
+
+
+})();
